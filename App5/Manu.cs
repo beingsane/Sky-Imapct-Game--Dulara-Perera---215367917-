9b539db1944a839f8b7bb5cbcb3c73b4de9a10dc
dularaperera <gdperera@deakin.edu.au>
2016-08-05 10:27:16 +1000

    updating Manu

--- App5/Manu.cs
+++ App5/Manu.cs
@@ -21,10 +21,10 @@ namespace App5
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
             button = FindViewById<Button>(Resource.Id.StartGame);
-            button.Click += newGame;
+            //button.Click += newGame;
 
             FindViewById<Button>(Resource.Id.about_btn).Click += about;
-            //FindViewById<Button>(Resource.Id.exit_btn).Click += exit;
+            FindViewById<Button>(Resource.Id.exit_btn).Click += exit;
         }
         void newGame(object sender, EventArgs e)
         {
